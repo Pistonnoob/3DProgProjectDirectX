8d00556263a0a28819cc4e4154679224480cd9dd
Pistonnoob <kimeld@hotmail.com>
2016-02-22 21:30:31 +0100

    Added more descriptions

--- ProjectDirectX/StructLibrary.h
+++ ProjectDirectX/StructLibrary.h
@@ -103,11 +103,11 @@ struct ObjMaterial
 //	{ "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
 //};
 
-//The one using a texture
+//Material description using Position and UV
 static const D3D11_INPUT_ELEMENT_DESC INPUT_DESC_UV[] = {
 	{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
 	{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-};
+};	//D3D11_APPEND_ALIGNED_ELEMENT is set so new elements are pushed right at the end of the last instead of a static variable
 
 //The one using both a UV and a Normal
 static const D3D11_INPUT_ELEMENT_DESC INPUT_DESC_3D[] = {
