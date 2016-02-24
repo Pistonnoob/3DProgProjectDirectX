f81edcf651fedd87f5d1cda8f156d34f3c6530ed
Knase23 <Jesper.uddefors.95@gmail.com>
2016-02-24 18:29:17 +0100

    Iteration to make the flat map
    Setting all positions to make a grid by 100x100. These numbers should be
    change depending on how big of a height map is loaded in.

--- ProjectDirectX/HeightMap.cpp
+++ ProjectDirectX/HeightMap.cpp
@@ -14,17 +14,25 @@ HeightMap::~HeightMap()
 
 bool HeightMap::CreateFlatMesh()
 {
-	Vector2 size = Vector2(100,100);
-	this->SetVertexCount(size.x * size.y);
+	int width = 100;
+	int depth = 100;
+	this->SetVertexCount(width * depth);		  // 100 x 100 = 10 000 
 	VertexModel* vertices = new VertexModel[this->GetVertexCount()];
 	unsigned long* indices = NULL;
 	D3D11_BUFFER_DESC vertexBufferDesc, indexBufferDesc;
 	D3D11_SUBRESOURCE_DATA vertexData, indexData;
 #pragma region
 	//Setting position for all vertices
-	
-
-#pragma endregion VerticesCreation 
+	int z = 0;
+	// x = 1 Becuase we want the first vertices to be (0,0,0)
+	for (int x = 1; x < this->GetVertexCount(); x++) 
+	{
+		if (x % 100 == 0)
+			z++;
+		vertices[x].position.x = x % 100;
+		vertices[x].position.z = z;
+	}
+#pragma endregion VerticesPositions 
 
 	return false;
 }
