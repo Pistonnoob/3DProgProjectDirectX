2990db8de589e7c3e065d29c97869e6258305a00
Pistonnoob <kimeld@hotmail.com>
2016-02-28 18:47:43 +0100

    Added speculare color

--- ProjectDirectX/GraphicsHandler.cpp
+++ ProjectDirectX/GraphicsHandler.cpp
@@ -229,7 +229,7 @@ bool GraphicsHandler::LoadScene(HWND hwnd)
 	m_Camera->SetPosition(0.0f, 0.0f, -5.0f);
 
 	//Create the lights.
-	m_Light = {Vector4(50, 50, 50, 1.0f), Vector4(255, 255, 255, 1.0f), Vector4(0, 5, -10, 1)/*, Vector4(0, 0, 1, 0)*/};
+	m_Light = { Vector4(50.0f, 50.0f, 50.0f, 1.0f), Vector4(255.0f, 255.0f, 255.0f, 1.0f), Vector4(255.0f, 255.0f, 255.0f, 1.0f), Vector4(0.0f, 5.0f, -10.0f, 1.0f)/*, Vector4(0, 0, 1, 0)*/ };
 
 	// Create the model objects.
 	//Test factory creating model objects.
