bbdffeba017bdc2989833f3a4ac7009f62d2895b
Pistonnoob <kimeld@hotmail.com>
2016-03-23 19:09:59 +0100

    trying to understand the deferred pixel shader

--- ProjectDirectX/LightPixelShader.hlsl
+++ ProjectDirectX/LightPixelShader.hlsl
@@ -3,21 +3,36 @@
 
 Texture2D colorTex : register(t0);
 Texture2D normalTex : register(t1);
-Texture2D positionTex : register(t2);
-Texture2D specularTex : register(t3);
-
+//Texture2D positionTex : register(t2);
+//Texture2D specularTex : register(t3);
 
 SamplerState samplerType : register(s0);
+
+cbuffer LightBuffer
+{
+	float3 lightPos;
+	float padding1;
+	float3 lightColor;
+	float padding2;
+};
 
 float4 main(PS_IN_LIGHT input) : SV_TARGET
 {
 	float4 result = (float4)0;
+	float4 additionColor = (float4)0;
 	//Get the color from the G_Buffer
-	float3 color = colorTex.Sample(samplerType, input.UV);
-	float3 normal = normalTex.Sample(samplerType, input.UV);
-	float3 position = positionTex.Sample(samplerType, input.UV);
-	float3 specular = specularTex.Sample(samplerType, input.UV);
-
+	float4 color = colorTex.Sample(samplerType, input.UV);
+	float4 normal = normalTex.Sample(samplerType, input.UV);
+	//float3 position = positionTex.Sample(samplerType, input.UV);
+	/*float3 specular = specularTex.Sample(samplerType, input.UV);*/
+	float3 lightDirection = -lightPos;
+	float lightIntensity = saturate(dot(lightDirection, (float3)input.Normal));
+	float4 diffuseResult = saturate((lightColor / 255) * lightIntensity);
+	if (lightIntensity > 0.0f)
+	{
+		//diffuseResult *= float4(material.Kd, 1.0f);
+		additionColor += diffuseResult;
+	}
 
 	return result;
 }
